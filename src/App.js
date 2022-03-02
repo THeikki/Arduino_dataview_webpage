2b5bda24da1489d5c12d07ebe84654554e0e069f
THeikki <t9tohe00@students.oamk.fi>
2022-03-02 13:38:32 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,48 +1,15 @@
 import './App.css';
-import axios from 'axios';
-import React, {useState, useEffect} from 'react';
 import Temperatures from './components/Temperatures';
-//import { useState } from 'react';
 
 function App() {
-
-  const [temperatures, setTemperature] = useState([])
-
-  useEffect(() => {
-    getTemperatures()
-  },)
-
-  const getTemperatures = () => {
-    axios
-      .get("http://localhost:5000/api/arduinoData/temperatures")
-      .then(res => {
-        setTemperature(res.data)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-
-  }
-  
-  const deleteTemperature = (id) => {
-    
-    axios.delete("http://localhost:5000/api/arduinoData/temperatures/" + id)
-    .then(res => {
-      console.log(res.data)
-      //getPersons()
-    })
-    .catch(err => {
-      console.log(err)
-
-    })
-  }
-
-
+ 
   return (
-    <div className="App">
-      <Temperatures temperatures={temperatures} handleClick={deleteTemperature}/>
+    <div>
+      <div className='temperatureBox'>
+        <Temperatures />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Temperatures.js
+++ src/components/Temperatures.js
@@ -1,6 +1,38 @@
 import Temperature from "./Temperature"
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-const Temperatures = ({ temperatures, handleClick }) => {
+const Temperatures = () => {
+
+    const [temperatures, setTemperature] = useState([])
+
+    useEffect(() => {
+        getTemperatures()
+    },)
+
+    const getTemperatures = () => {
+        axios
+        .get("http://localhost:5000/api/arduinoData/temperatures")
+        .then(res => {
+            setTemperature(res.data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
+    }
+    
+    const deleteTemperature = (id) => {
+        
+        axios.delete("http://localhost:5000/api/arduinoData/temperatures/" + id)
+        .then(res => {
+            console.log(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+    
     if(temperatures.length === 0) {
         return (
             <div>
@@ -11,9 +43,10 @@ const Temperatures = ({ temperatures, handleClick }) => {
 
     return (
         <div>
+            <h1>Arduino lämpötilat</h1>
             {
                 temperatures.map(temperature =>
-                    <Temperature key={temperature._id} temperature={temperature} handleClick={handleClick}/>
+                    <Temperature key={temperature._id} temperature={temperature} handleClick={deleteTemperature}/>
                 )
             }
         </div>
